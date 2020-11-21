01bf23030c5d58c2dd5179eb6b5d1f9a8a19b961
Nathan Spindel <42874502+nspindel@users.noreply.github.com>
2020-11-21 10:45:40 -0500

    2020-11-21 NS: Cleaned up code for archive. Removed unneccessary metadata.

--- 2_Analysis_Code/Analysis_Code.R
+++ 2_Analysis_Code/Analysis_Code.R
@@ -1,6 +1,6 @@
 ###############################################################################
-# Analysis code for "Metabolic depression in sea urchin barrens associated    #
-# with food deprivation."                                                     #
+# Analysis code for "Zombies of the Nearshore: metabolic depression in sea    #
+# urchin barrens associated with food deprivation."                           #                          #
 #                                                                             #
 # Authors: N_B_ Spindel & D_K_ Okamoto                                        #
 ###############################################################################
@@ -16,8 +16,6 @@ library(MuMIn)
 library(Hmisc)
 library(car)
 library(cowplot)
-library(sjPlot)
-library(xtable)
 
 # Load functions --------------------------------------------------------------
 source("./2_Analysis_Code/Functions.R")
@@ -79,32 +77,29 @@ dat_filtered_AFDM <- dat_filtered %>%
 dat_filtered_SN <- dat_filtered %>%
   filter(site == "Surge Narrows")
 
-# RMR Analysis (Test Volume): effects of site and habitat  --------------------
+# RMR Analysis (Wet Mass): effects of site and habitat  --------------------
 # Model selection: Fit several reasonable GLMMs for fixed effects on volumetric
 # oxygen consumption rate using Restricted Maximum Likelihood. 
-f1 <- glmmTMB(volumetric_rate ~ log(spheroid.volume.ml) + (1 | date/chamber_id), 
+f1 <- glmmTMB(volumetric_rate ~ log(animal_wet_mass_g) + (1 | date/chamber_id), 
               data = dat_filtered,
               family = gaussian(link = "log"),
               REML = TRUE)
-f2 <- update(f1, formula = . ~ log(spheroid.volume.ml) + habitat + (1 | date/chamber_id))
-f3 <- update(f1, formula = . ~ log(spheroid.volume.ml) + habitat + site + (1 | date/chamber_id))
-f4 <- update(f1, formula = . ~ log(spheroid.volume.ml) * habitat + site + (1 | date/chamber_id))
-f5 <- update(f1, formula = . ~ log(spheroid.volume.ml) + habitat * site + (1 | date/chamber_id))
-f6 <- update(f1, formula = . ~ log(spheroid.volume.ml) * habitat * site + (1 | date/chamber_id))
-f7 <- update(f1, formula = . ~ log(spheroid.volume.ml) : habitat + site + (1 | date/chamber_id))
-f8 <- update(f1, formula = . ~ log(spheroid.volume.ml) : habitat : site + (1 | date/chamber_id))
-f9 <- update(f1, formula = . ~ log(spheroid.volume.ml) : habitat * site + (1 | date/chamber_id))
+f2 <- update(f1, formula = . ~ log(animal_wet_mass_g) + habitat + (1 | date/chamber_id))
+f3 <- update(f1, formula = . ~ log(animal_wet_mass_g) + habitat + site + (1 | date/chamber_id))
+f4 <- update(f1, formula = . ~ log(animal_wet_mass_g) * habitat + site + (1 | date/chamber_id))
+f5 <- update(f1, formula = . ~ log(animal_wet_mass_g) + habitat * site + (1 | date/chamber_id))
+f6 <- update(f1, formula = . ~ log(animal_wet_mass_g) * habitat * site + (1 | date/chamber_id))
+f7 <- update(f1, formula = . ~ log(animal_wet_mass_g) : habitat + site + (1 | date/chamber_id))
+f8 <- update(f1, formula = . ~ log(animal_wet_mass_g) : habitat : site + (1 | date/chamber_id))
+f9 <- update(f1, formula = . ~ log(animal_wet_mass_g) : habitat * site + (1 | date/chamber_id))
 
 # Calculate Second-order Akaike Information Criterion for various model fits.
 AICc(f1, f2, f3, f4, f5, f6, f7, f8, f9)
 
-# Although f3 has the lowest AICc, we select f6 to examine habitat:body size  and habitat:site interactions. Re-fit selected model using 
-# Maximum Likelihood for inference.
-f6 <- update(f6, REML = FALSE)
-fit_glmmTMB_vo2_vol_site <- f6
-
+f5 <- update(f5, REML = FALSE)
+fit_glmmTMB <- f5
 # Model diagnostics
-simulation_output <- simulateResiduals(fit_glmmTMB_vo2_vol_site, n = 250)
+simulation_output <- simulateResiduals(fit_glmmTMB, n = 250)
 
 # Plot scaled residuals:
 plot(simulation_output)
@@ -113,44 +108,44 @@ plot(simulation_output)
 testResiduals(simulation_output)
 
 # Run Analysis of Deviance (Type III Wald chisquare tests)
-red_vo2_vol_site_anova <- glmmTMB:::Anova.glmmTMB(
-  fit_glmmTMB_vo2_vol_site,
+wet_mass_x_site_anova <- glmmTMB:::Anova.glmmTMB(
+  fit_glmmTMB,
   type = c("III"),
   test.statistic = c("Chisq"),
   component = "cond"
 )
 
 # Render test results
-red_vo2_vol_site_anova
+wet_mass_x_site_anova
 
 # Visualize predictions and data 
-fixed_predictions_vol_site <- predict(object = fit_glmmTMB_vo2_vol_site, 
+fixed_predictions <- predict(object = fit_glmmTMB, 
                                  newdata = dat_filtered,
                                  re.form = ~ 0,
                                  se.fit = TRUE)
-dat_filtered$pred_fixed_vol_site <- exp(fixed_predictions_vol_site$fit)
-dat_filtered$pred_fixed_se_vol_site_lci <- exp(fixed_predictions_vol_site$fit-
-                                                          fixed_predictions_vol_site$se.fit)
-dat_filtered$pred_fixed_se_vol_site_uci <- exp(fixed_predictions_vol_site$fit+
-                                                          fixed_predictions_vol_site$se.fit)
+dat_filtered$pred_fixed_fit <- exp(fixed_predictions$fit)
+dat_filtered$pred_fixed_se_lci <- exp(fixed_predictions$fit-
+                                                 fixed_predictions$se.fit)
+dat_filtered$pred_fixed_se_uci <- exp(fixed_predictions$fit+
+                                                 fixed_predictions$se.fit)
 
-p_vo2_vol_site_red <- ggplot(aes(x = spheroid.volume.ml,
+p_wet_mass_x_site <- ggplot(aes(x = animal_wet_mass_g,
                             y = volumetric_rate), data = dat_filtered)+
   geom_point(aes(color = habitat, shape = habitat), size = 3, alpha = 0.5)+
-  geom_line(aes(y = pred_fixed_vol_site, color = habitat))+
-  geom_ribbon(aes(ymin = pred_fixed_se_vol_site_lci, 
-                  ymax = pred_fixed_se_vol_site_uci, fill = habitat), alpha = 0.5)+
+  geom_line(aes(y = pred_fixed_fit, color = habitat))+
+  geom_ribbon(aes(ymin = pred_fixed_se_lci, 
+                  ymax = pred_fixed_se_uci, fill = habitat), alpha = 0.5)+
   scale_color_manual(values = plot_colors)+
   scale_fill_manual(values = plot_colors)+
   scale_x_continuous(trans = "log10")+
   annotation_logticks(sides = "b")+
   facet_wrap(vars(site))+
-  xlab("Test Volume (mL)")+
+  xlab("Wet Mass (g)")+
   labs(y=expression(VO["2"]*" (mgO"["2"]*"h"^-1*")"))+
   gg_options()+
   theme(legend.position = c(0.1,0.8));
 
-p_vo2_vol_site_red
+p_wet_mass_x_site
 
 # RMR Analysis (Total Ash-Free Dry Mass): effect of habitat  ------------------------
 # Model selection: Fit several reasonable GLMMs for fixed effects on volumetric
@@ -167,13 +162,13 @@ f4 <- update(f1, formula = . ~ log(whole.afdm.g.approx) : habitat + (1 | date/ch
 # Calculate Second-order Akaike Information Criterion for various model fits.
 AICc(f1, f2, f3, f4)
 
-# Although f2 has the lowest AICc, we select f3 to examine habitat:body size interaction. Re-fit selected model using 
-# Maximum Likelihood for inference.
+# Although f2 has the lowest AICc, we select f3 to examine habitat:body size interaction. 
+# Re-fit selected model using Maximum Likelihood for inference.
 f3 <- update(f3, REML = FALSE)
-fit_glmmTMB_vo2_afdm <- f3
+fit_glmmTMB <- f3
 
 # Model diagnostics
-simulation_output <- simulateResiduals(fit_glmmTMB_vo2_afdm, n = 250)
+simulation_output <- simulateResiduals(fit_glmmTMB, n = 250)
 
 # Plot scaled residuals:
 plot(simulation_output)
@@ -182,33 +177,33 @@ plot(simulation_output)
 testResiduals(simulation_output)
 
 # Run Analysis of Deviance (Type III Wald chisquare tests)
-red_vo2_afdm_anova <- glmmTMB:::Anova.glmmTMB(
-  fit_glmmTMB_vo2_afdm,
+afdm_anova <- glmmTMB:::Anova.glmmTMB(
+  fit_glmmTMB,
   type = c("III"),
   test.statistic = c("Chisq"),
   component = "cond"
 )
 
 # Render test results
-red_vo2_afdm_anova
+afdm_anova
 
 # Visualize predictions and data 
-fixed_predictions_afdm <- predict(object = fit_glmmTMB_vo2_afdm, 
+fixed_predictions <- predict(object = fit_glmmTMB, 
                                       newdata = dat_filtered_AFDM,
                                       re.form = ~ 0,
                                       se.fit = TRUE)
-dat_filtered_AFDM$pred_fixed_afdm <- exp(fixed_predictions_afdm$fit)
-dat_filtered_AFDM$pred_fixed_se_afdm_lci <- exp(fixed_predictions_afdm$fit-
-                                                 fixed_predictions_afdm$se.fit)
-dat_filtered_AFDM$pred_fixed_se_afdm_uci <- exp(fixed_predictions_afdm$fit+
-                                                 fixed_predictions_afdm$se.fit)
+dat_filtered_AFDM$pred_fixed_fit <- exp(fixed_predictions$fit)
+dat_filtered_AFDM$pred_fixed_se_lci <- exp(fixed_predictions$fit-
+                                                 fixed_predictions$se.fit)
+dat_filtered_AFDM$pred_fixed_se_uci <- exp(fixed_predictions$fit+
+                                                 fixed_predictions$se.fit)
 
-p_vo2_afdm <- ggplot(aes(x = whole.afdm.g.approx, 
+p_afdm <- ggplot(aes(x = whole.afdm.g.approx, 
                                  y = volumetric_rate), data = dat_filtered_AFDM)+
   geom_point(aes(color = habitat, shape = habitat), size = 3, alpha = 0.5)+
-  geom_line(aes(y = pred_fixed_afdm, color = habitat))+
-  geom_ribbon(aes(ymin = pred_fixed_se_afdm_lci, 
-                  ymax = pred_fixed_se_afdm_uci, fill = habitat), alpha = 0.5)+
+  geom_line(aes(y = pred_fixed_fit, color = habitat))+
+  geom_ribbon(aes(ymin = pred_fixed_se_lci, 
+                  ymax = pred_fixed_se_uci, fill = habitat), alpha = 0.5)+
   scale_color_manual(values = plot_colors)+
   scale_fill_manual(values = plot_colors)+
   scale_x_continuous(trans = "log10")+
@@ -218,7 +213,7 @@ p_vo2_afdm <- ggplot(aes(x = whole.afdm.g.approx,
   gg_options()+
   theme(legend.position = "");
 
-p_vo2_afdm
+p_afdm
 
 # RMR Analysis (Gonadal Ash-Free Dry Mass): effect of habitat  ----------------
 # Model selection: Fit several reasonable GLMMs for fixed effects on volumetric
@@ -237,10 +232,10 @@ AICc(f1, f2, f3, f4)
 # Select model f3 based on having lowest AICc. Re-fit selected model using 
 # Maximum Likelihood for inference.
 f3 <- update(f3, REML = FALSE)
-fit_glmmTMB_vo2_afdm_gonad <- f3
+fit_glmmTMB <- f3
 
 # Model diagnostics
-simulation_output <- simulateResiduals(fit_glmmTMB_vo2_afdm_gonad, n = 250)
+simulation_output <- simulateResiduals(fit_glmmTMB, n = 250)
 
 # Plot scaled residuals:
 plot(simulation_output)
@@ -249,33 +244,33 @@ plot(simulation_output)
 testResiduals(simulation_output)
 
 # Run Analysis of Deviance (Type III Wald chisquare tests)
-red_vo2_afdm_gonad_anova <- glmmTMB:::Anova.glmmTMB(
-  fit_glmmTMB_vo2_afdm_gonad,
+gonad_afdm_anova <- glmmTMB:::Anova.glmmTMB(
+  fit_glmmTMB,
   type = c("III"),
   test.statistic = c("Chisq"),
   component = "cond"
 )
 
 # Render test results
-red_vo2_afdm_gonad_anova
+gonad_afdm_anova
 
 # Visualize predictions and data 
-fixed_predictions_afdm_gonad <- predict(object = fit_glmmTMB_vo2_afdm_gonad, 
+fixed_predictions <- predict(object = fit_glmmTMB, 
                                   newdata = dat_filtered_AFDM,
                                   re.form = ~ 0,
                                   se.fit = TRUE)
-dat_filtered_AFDM$pred_fixed_afdm_gonad <- exp(fixed_predictions_afdm_gonad$fit)
-dat_filtered_AFDM$pred_fixed_se_afdm_gonad_lci <- exp(fixed_predictions_afdm_gonad$fit-
-                                                  fixed_predictions_afdm_gonad$se.fit)
-dat_filtered_AFDM$pred_fixed_se_afdm_gonad_uci <- exp(fixed_predictions_afdm_gonad$fit+
-                                                  fixed_predictions_afdm_gonad$se.fit)
+dat_filtered_AFDM$pred_fixed_fit <- exp(fixed_predictions$fit)
+dat_filtered_AFDM$pred_fixed_se_lci <- exp(fixed_predictions$fit-
+                                                        fixed_predictions$se.fit)
+dat_filtered_AFDM$pred_fixed_se_uci <- exp(fixed_predictions$fit+
+                                                        fixed_predictions$se.fit)
 
-p_vo2_afdm_gonad <- ggplot(aes(x = gonad.afdm.g.total.approx, 
+p_gonad_afdm <- ggplot(aes(x = gonad.afdm.g.total.approx, 
                          y = volumetric_rate), data = dat_filtered_AFDM)+
   geom_point(aes(color = habitat, shape = habitat), size = 3, alpha = 0.5)+
-  geom_line(aes(y = pred_fixed_afdm_gonad, color = habitat))+
-  geom_ribbon(aes(ymin = pred_fixed_se_afdm_gonad_lci, 
-                  ymax = pred_fixed_se_afdm_gonad_uci, fill = habitat), alpha = 0.5)+
+  geom_line(aes(y = pred_fixed_fit, color = habitat))+
+  geom_ribbon(aes(ymin = pred_fixed_se_lci, 
+                  ymax = pred_fixed_se_uci, fill = habitat), alpha = 0.5)+
   scale_color_manual(values = plot_colors)+
   scale_fill_manual(values = plot_colors)+
   scale_x_continuous(trans = "log10")+
@@ -285,12 +280,12 @@ p_vo2_afdm_gonad <- ggplot(aes(x = gonad.afdm.g.total.approx,
   gg_options()+
   theme(legend.position = "");
 
-p_vo2_afdm_gonad
+p_gonad_afdm
 
 # Combine RMR regression visualizations ---------------------------------------
 # 3-panel figure with RMR regressed against test volume, total AFDM, and 
 # gonadal AFDM by habitat for urchins from Surge Narrows
-RMR_multiplot_SN <- cowplot::plot_grid(p_vo2_afdm, p_vo2_afdm_gonad,
+RMR_multiplot_SN <- cowplot::plot_grid(p_afdm, p_gonad_afdm,
                                      ncol = 2,
                                      labels = c('A', 'B'), 
                                      label_size = 25)
@@ -298,7 +293,7 @@ RMR_multiplot_SN
 
 # 3-panel figure with RMR regressed against test volume by habitat and site for 
 # urchins from Faraday Island, Murchison Island, and Surge Narrows
-RMR_multiplot_hab_x_site <- cowplot::plot_grid(RMR_multiplot_SN, p_vo2_vol_site_red,
+RMR_multiplot_hab_x_site <- cowplot::plot_grid(RMR_multiplot_SN, p_wet_mass_x_site,
                               ncol = 1,
                               # labels = c('', 'C'), 
                               label_size = 25)
@@ -327,10 +322,10 @@ AICc(f1, f2, f3, f4, f5, f6, f7, f8, f9)
 # Select model f4 based on having lowest AICc. Re-fit selected model using 
 # Maximum Likelihood for inference.
 f4 <- update(f4, REML = FALSE)
-fit_glmmTMB_gonad_vol_site <- f4
+fit_glmmTMB <- f4
 
 # Model diagnostics
-simulation_output <- simulateResiduals(fit_glmmTMB_gonad_vol_site, n = 250)
+simulation_output <- simulateResiduals(fit_glmmTMB, n = 250)
 
 # Plot scaled residuals:
 plot(simulation_output)
@@ -339,12 +334,41 @@ plot(simulation_output)
 testResiduals(simulation_output)
 
 # Run Analysis of Deviance (Type III Wald chisquare tests)
-red_gonad_vol_site_anova <- glmmTMB:::Anova.glmmTMB(
-  fit_glmmTMB_gonad_vol_site,
+gonad_anova <- glmmTMB:::Anova.glmmTMB(
+  fit_glmmTMB,
   type = c("III"),
   test.statistic = c("Chisq"),
   component = "cond"
 )
 
 # Render test results
-red_gonad_vol_site_anova
+gonad_anova
+
+# Visualize predictions and data 
+fixed_predictions <- predict(object = fit_glmmTMB, 
+                             newdata = dat_filtered,
+                             re.form = ~ 0,
+                             se.fit = TRUE)
+dat_filtered$pred_fixed_fit <- exp(fixed_predictions$fit)
+dat_filtered$pred_fixed_se_lci <- exp(fixed_predictions$fit-
+                                             fixed_predictions$se.fit)
+dat_filtered$pred_fixed_se_uci <- exp(fixed_predictions$fit+
+                                             fixed_predictions$se.fit)
+
+p_gonad <- ggplot(aes(x = spheroid.volume.ml, 
+                           y = total_gonad_approx_g), data = dat_filtered)+
+  geom_point(aes(color = habitat, shape = habitat), size = 3, alpha = 0.5)+
+  geom_line(aes(y = pred_fixed_fit, color = habitat))+
+  geom_ribbon(aes(ymin = pred_fixed_se_lci,
+                  ymax = pred_fixed_se_uci, fill = habitat), alpha = 0.5)+
+  scale_color_manual(values = plot_colors)+
+  scale_fill_manual(values = plot_colors)+
+  scale_x_continuous(trans = "log10")+
+  annotation_logticks(sides = "b")+
+  facet_wrap(vars(site))+
+  xlab("Test Volume (mL)")+
+  labs(y=expression(VO["2"]*" (mgO"["2"]*"h"^-1*")"))+
+  gg_options()+
+  theme(legend.position = "");
+
+p_gonad
